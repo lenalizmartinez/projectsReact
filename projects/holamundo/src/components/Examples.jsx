6f07c564411cba527dab7e8867a0130bf572c300
lenalizmartinez <lenalizmartinez@gmail.com>
2023-12-19 23:59:24 -0500

    refactoring the project

--- projects/holamundo/src/components/Examples.jsx
+++ projects/holamundo/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data.js";
 import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
 
 
 export default function Examples() {
@@ -27,36 +28,36 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
+    <Section title="Examples" id="examples">
       <h2>Examples</h2>
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handSelect("components")}
+          onClick={() => handSelect("components")}
         >
           Component
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handSelect("jsx")}
+          onClick={() => handSelect("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handSelect("props")}
+          onClick={() => handSelect("props")}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handSelect("state")}
+          onClick={() => handSelect("state")}
         >
           State
         </TabButton>
       </menu>
 
       {tabContent}
-    </section>
+    </Section>
   );
 }
--- projects/holamundo/src/components/TabButton.jsx
+++ projects/holamundo/src/components/TabButton.jsx
@@ -1,10 +1,13 @@
-export default function TabButton({children, onSelect, isSelected}){
-    console.log('APP tabButton executing');
-    return (
-        <li><button className={isSelected ? "active" : undefined } onClick={onSelect}>{children}</button></li>
-    );
+export default function TabButton({ children, isSelected, ...props }) {
+  console.log("APP tabButton executing");
+  return (
+    <li>
+      <button className={isSelected ? 'active' : undefined} {...props}>
+        {children}
+      </button>
+    </li>
+  );
 }
-
 
 // cualquiera de las dos funciones realiza la misma accion
 // export default function TabButton({children}}){
