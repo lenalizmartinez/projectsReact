fadb9f021a588eb8c1d34aed4733f522052c8c21
lenalizmartinez <lenalizmartinez@gmail.com>
2023-12-26 14:58:23 -0500

    ready

---
+++ projects/refs-starting-project/src/components/ResultModal.jsx
@@ -0,0 +1,32 @@
+import { forwardRef, useRef, useImperativeHandle } from "react";
+
+const ResultModal = forwardRef(function resultModal(
+  { targetTime, remainingTime, onReset }, ref) {
+  
+  const dialog = useRef();
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = (remainingTime/(targetTime * 100) *100);
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2> You lost</h2>}
+      {!userLost && <h2> You score: {score}</h2>}
+    
+      <p>The target time was <strong>{targetTime} seconds.</strong></p>
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left .</strong></p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
